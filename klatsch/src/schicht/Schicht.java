1aab8374cf58c96164db4d92fc2109cc05fe211c
cemo <cemo@10.51.195.0>
2016-01-26 15:04:57 +0100

    schichtrechner gelöscht

--- klatsch/src/schicht/Schicht.java
+++ klatsch/src/schicht/Schicht.java
@@ -43,66 +43,6 @@ public abstract class Schicht {
 		return sb.toString();
 		
 	}
-	public void schictrechner(String[] args){
-		int [][] times = new int [2][2];
-		double schichtdauer = 0;
-		
-		if (args.length == 2){
-			times = myTime (args[0], args[1]);
-			if (times[0][0]== 0 && times [0] [1]==0 && times [1] [0] == 0 && 
-					times [1] [1] == 0){
-				schichtdauer = 0;
-			} else{
-				schichtdauer = MyArbeitsTime(times);
-				System.out.println("schict dauert" + schichtdauer + "Stunden");
-			}
-		}
-			else {
-				System.out.println("falsche eingabe bitte versuchen sie nochmal");
-			}
-		}
-		
-		
-
 	
-	private double MyArbeitsTime(int[][] times) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	private static int[][] myTime(String begin, String end){
-		int[] [] Arbeitszeit = new int [2] [2];
-		
-		String[] ergebnis1 = begin.split(":");
-		String[] ergebnis2 = end.split(":");
-		
-		int begin1 = Integer.valueOf(ergebnis1[0]). intValue();
-		int begin2 = Integer.valueOf(ergebnis1[1]). intValue();
-		int end1 = Integer.valueOf(ergebnis2[0]). intValue();
-		int end2 = Integer.valueOf(ergebnis2[1]). intValue();
-		
-		Arbeitszeit[0][0] = begin1;
-		Arbeitszeit[0][1] = begin2;
-		Arbeitszeit[0][0] = end1;
-		Arbeitszeit[0][1] = end2;
-		
-		return Arbeitszeit;
-		
-	}
-	public static void MyArbeitsTime1(int[] [] Arbeitszeit){
-		boolean nachtschicht;
-		
-		int zahl1 = (Arbeitszeit [0] [0] * 60) + Arbeitszeit [0] [1];
-		int zahl2 = (Arbeitszeit [1] [0] * 60) + Arbeitszeit [1] [1];
-		
-		int zErg = 0;
-		zErg = zahl1 <= zahl2? zahl2 - zahl1 : ((24*60)-zahl1)+ zahl2;
-		nachtschicht = zahl1 <= zahl2 ? false : true;
-		
-		
-		
-	}
-
-
 	public abstract String getBeschreibung();
 }
